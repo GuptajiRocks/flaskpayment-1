99ebc542b35b652b55a4a228a2e2ebff65f7fe9e
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-24 21:14:14 +0530

    azure SQL configured

--- testdb.py
+++ testdb.py
@@ -9,10 +9,9 @@ username = 'arihant'
 password = os.environ["SQLPSWD"]
 driver= '{ODBC Driver 18 for SQL Server}'
 
-with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-        row = cursor.fetchone()
-        while row:
-            print (str(row[0]) + " " + str(row[1]))
-            row = cursor.fetchone()
+conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+curs = conn.cursor()
+
+curs.execute("SELECT name FROM master.dbo.sysdatabases")
+res = curs.fetchall()
+print(res)
