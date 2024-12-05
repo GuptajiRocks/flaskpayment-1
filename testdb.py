49f56e0f18513a3edc6cb8f59f69c8bcf881c3d4
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-05 17:12:03 +0530

    env working and azure connection established

--- testdb.py
+++ testdb.py
@@ -1,17 +1,18 @@
 import pyodbc
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 server = 'arihantsqldbs.database.windows.net'
 database = 'freeDB'
 username = 'arihant'
-password = os.environ["SQLPSWD"]
+password = os.getenv("SQLPSWD")
 driver= '{ODBC Driver 18 for SQL Server}'
 
 conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 curs = conn.cursor()
 
-curs.execute("SELECT name FROM master.dbo.sysdatabases")
+curs.execute("SELECT name, database_id, create_date FROM sys.databases;")
 res = curs.fetchall()
 print(res)
